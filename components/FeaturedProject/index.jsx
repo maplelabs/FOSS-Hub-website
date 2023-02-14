819e0d102cda08a002a0d734f47ba92a420d1537
Aishik Mukherjee <Aishik.Mukherjee@maplelabs.com>
2023-02-14 17:06:03 +0530

    Added projects table for featured projects

--- components/FeaturedProject/index.jsx
+++ components/FeaturedProject/index.jsx
@@ -2,9 +2,11 @@ import Link from 'next/link';
 import React, { useEffect } from 'react';
 import styles from "../../styles/Home.module.css";
 import ProjectCard from '../Project';
+import ProjectsTable from '../ProjectsTable';
 
 
 export default function FeaturedProject({data}){
+  const [tabularView, setTabularView] = React.useState(true);
     return (
       <div id='projects' className="uk-width-1-1">
           <div className="uk-section uk-dark" style={{marginBottom:'-20px'}}>
@@ -18,10 +20,21 @@ export default function FeaturedProject({data}){
                   VIEW ALL
                   </Link>
                 </div>
-              </div>    
+                <div>
+                <button
+                  className="tabView"
+                  type="button"
+                  uk-icon={tabularView ? "icon: list" : "icon:thumbnails"}
+                  onClick={() => setTabularView(!tabularView)}
+                ></button>
+                </div>
+              </div>
+              {tabularView ? ( 
               <div className="uk-child-width-1-4@m uk-text-left uk-grid uk-grid-small uk-grid-match" data-uk-grid>
                   {data.map(project=><ProjectCard data={project} key={project.id}></ProjectCard>)}
-              </div>
+              </div> ) : (
+                <ProjectsTable data={data} key={data.id}></ProjectsTable>
+              )}
             </div>
           </div>
         </div>
---
+++ components/ProjectsTable/index.jsx
@@ -0,0 +1,115 @@
+import Link from "next/link";
+
+export default function ProjectsTable({ data }) {
+  
+    return (
+      <div>
+        <table className="uk-table uk-table-divider ">
+          <thead>
+            <tr>
+              <th>Project Name</th>
+              <th>Tech Stack</th>
+              <th>Category</th>
+              <th>Tags</th>
+              <th>Contributors</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row) => (
+              <tr>
+                <td>
+                 <Link target="_blank" rel="noopener noreferrer" href={row.html_url} className="hover">
+                  <div className=" uk-flex uk-flex-row">
+                    <div className=" uk-margin-small-right">
+                      <img
+                        className="uk-border-circle "
+                        src={row.icon_url}
+                        width="40"
+                        height="40"
+                        alt=""
+                      ></img>
+                    </div>
+                    <div>
+                      <h3 className="uk-h4 uk-text-bolder uk-margin-small-bottom">
+                        {row.name}
+                      </h3>
+                      <div className='uk-text-small color-lead uk-text-secondary uk-width-large'>
+                        {row.description}
+                      </div>
+                    </div>
+                  </div>
+                  </Link>
+                </td>
+                <td>
+                  <div className=" uk-flex uk-flex-row ">
+                    {row.languages.map((Lang) => (
+                      <span
+                      className="mpl-badge uk-light uk-margin-small-bottom uk-margin-small-right"
+                      >
+                        {Lang}
+                      </span>
+                    ))}
+                  </div>
+                </td>
+                <td>
+                  <span className="mpl-badge uk-light uk-margin-small-bottom uk-margin-small-right">
+                    utilities
+                  </span>
+                </td>
+                <td className="uk-flex uk-flex-row">
+                  {row.topics.map((topic) => (
+                    <span
+                    className="mpl-badge uk-light uk-margin-small-bottom uk-margin-small-right"
+                    >
+                      {topic}
+                    </span>
+                  ))}
+                </td>
+                <td>
+                  <div className="uk-flex ">
+                    {row.top_contributors.map((user) => (
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        key={user.login}
+                        href={user.html_url}
+                      >
+                        <img
+                          className="uk-comment-avatar contributors uk-border-circle"
+                          uk-tooltip={`title: ${user.login}; pos: bottom`}
+                          src={user.avatar_url}
+                          width="25"
+                          height="25"
+                          alt=""
+                        />
+                      </a>
+                    ))}
+                    {/* <div>
+                      {row.contributors.length > 3 && (
+                        <button className="contribButton">
+                          {`+${row.contributors.length - 3}`}
+                        </button>
+                      )}
+                      <div uk-dropdown="pos: bottom-left; mode: click;boundary: !.contribButton">
+                        <ul className="uk-nav uk-dropdown-nav uk-text-bold">
+                          {row.contributors.slice(3).map((contrib) => (
+                            <li className="uk-margin-small-bottom">
+                              <div className="uk-flex">
+                              <img src={contrib.avatar_url} className="uk-margin-small-right additionalContributorsStyle"></img>
+                              <span>{contrib.login}</span>
+                              </div>
+                            </li>
+                            
+                          ))}
+                        </ul>
+                      </div>
+                    </div> */}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
