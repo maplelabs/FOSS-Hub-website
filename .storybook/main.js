26ab163203c0d9f56baa15bf113821f77f5338aa
Skanda <skanda.j@maplelabs.com>
2023-03-01 12:09:16 +0530

    Updated Storybook config for svg

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,4 +14,28 @@ module.exports = {
     builder: '@storybook/builder-webpack5',
   },
   staticDirs: ['../public'],
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(ttf|eot|svg)$/,
+  //       use: 'file-loader?name=[name].[ext]',
+  //       exclude: /\.inline.svg$/,
+  //     },
+  //     {
+  //       test: /\.inline.svg$/,
+  //       use: 'svgr/webpack',
+  //     },
+  //   ],
+  // },
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules = config.module.rules.map((rule) =>
+      rule.test.test('.svg') ? { ...rule, exclude: /\.svg$/ } : rule
+    );
+
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
